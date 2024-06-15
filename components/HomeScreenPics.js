0d52212e66141798081bf0076aaaeaa939b819de
delafuentej <jfuentelucas@gmail.com>
2024-06-15 22:07:54 +0200

    homescreenpics component added

---
+++ components/HomeScreenPics.js
@@ -0,0 +1,173 @@
+import { shuffleyArray } from "../utils/shuffleArray";
+
+export const HomeScreenPics = shuffleyArray([
+    {
+        pic: require('../assets/images/woman/woman1.png'),
+        title:'Anne, 30',
+        caption:'18 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman2.png'),
+        title:'Lisa, 24',
+        caption:'30 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman3.png'),
+        title:'Dayana, 31',
+        caption:'2 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman4.png'),
+        title:'Anabela, 27',
+        caption:'8 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman5.png'),
+        title:'Beatriz, 25',
+        caption:'12 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman6.png'),
+        title:'Jun, 33',
+        caption:'80 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman7.png'),
+        title:'Jia Li, 18',
+        caption:'200 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman8.png'),
+        title:'Amira, 37',
+        caption:'320 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman9.png'),
+        title:'Chetana, 39',
+        caption:'125 kms away',
+
+    },
+    {
+        pic: require('../assets/images/woman/woman10.png'),
+        title:'Paloma, 26',
+        caption:'23 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man1.png'),
+        title:'Mike, 22',
+        caption:'5 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man2.png'),
+        title:'Yoshio, 28',
+        caption:'3 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man3.png'),
+        title:'Stefan, 31',
+        caption:'120 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man4.png'),
+        title:'Santiago, 29',
+        caption:'80 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man5.png'),
+        title:'Charlie, 41',
+        caption:'345 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man6.png'),
+        title:'Tomasso, 37',
+        caption:'254 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man7.png'),
+        title:'Lethabo, 35',
+        caption:'78 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man8.png'),
+        title:'Hassan, 40',
+        caption:'500 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man9.png'),
+        title:'Jorge, 33',
+        caption:'220 kms away',
+
+    },
+    {
+        pic: require('../assets/images/man/man10.png'),
+        title:'Ari, 43',
+        caption:'897 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers1.png'),
+        title:'Nicole, 26',
+        caption:'99 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers2.png'),
+        title:'Alexia, 39',
+        caption:'67 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers3.png'),
+        title:'Damaris, 45',
+        caption:'108 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers4.png'),
+        title:'Nahuel, 22',
+        caption:'220 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers5.png'),
+        title:'Kevin, 25',
+        caption:'8 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers6.png'),
+        title:'Allen, 24',
+        caption:'23 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers7.png'),
+        title:'Ahmed, 34',
+        caption:'333 kms away',
+
+    },
+    {
+        pic: require('../assets/images/divers/divers8.png'),
+        title:'Alexis, 26',
+        caption:'78 kms away',
+
+    },
+
+]);
