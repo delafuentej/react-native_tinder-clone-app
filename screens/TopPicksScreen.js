666e7ba4219f25f2e07ae4ca991a72e514305e63
delafuentej <jfuentelucas@gmail.com>
2024-06-15 11:32:59 +0200

    screens folder added

---
+++ screens/TopPicksScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const TopPicksScreen=()=>{
+    return(
+        <View>
+            <Text>TopPicks Screen</Text>
+        </View>
+    )
+}
