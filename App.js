a2fac73bf50a192027b5dcad3c84d14aefd6e109
delafuentej <jfuentelucas@gmail.com>
2024-06-15 17:34:43 +0200

    google fonts into assets/fonts added & settings fonts in app.js

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { MainTabBottomNavigator } from './navigation/MainTabBottomNavigator';
+import { enableScreens } from 'react-native-screens';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+//to improve performance
+enableScreens();
+
+// to prevent the splash-screen from auto-hiding
+SplashScreen. preventAutoHideAsync();
+
+//google-fonts
+const getFonts=()=> Font.loadAsync({
+  'libre-baskerville-bold': require('./assets/fonts/LibreBaskerville-Bold.ttf'),
+  'libre-baskerville-regular-italic': require('./assets/fonts/LibreBaskerville-Italic.ttf'),
+  'libre-baskerville-regular': require('./assets/fonts/LibreBaskerville-Regular.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-     <MainTabBottomNavigator />
-    </View>
-  );
+  const[fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(()=>{
+    const loadResourcesAndDataAsync = async()=>{
+      try{
+        //to loading fonts
+        await getFonts();
+      }catch(error){
+        console.warn(error);
+      }finally{
+        setFontsLoaded(true);
+        //to hide splashScreen
+        SplashScreen.hideAsync();
+      };
+    };
+    loadResourcesAndDataAsync();
+  },[]);
+
+  if(!fontsLoaded){
+    return null;
+  }else{
+    return (
+      <View style={styles.container}>
+        <MainTabBottomNavigator />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +56,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'libre-baskerville-bold',
   },
 });
